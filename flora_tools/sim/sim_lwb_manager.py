9be85bfc43c924eb505339bd078661ac258bd9cc
Markus Wegmann <atokulus@gmail.com>
2018-07-05 19:15:52 +0200

    Clone stm32loader recursively

--- flora_tools/sim/sim_lwb_manager.py
+++ flora_tools/sim/sim_lwb_manager.py
@@ -10,7 +10,7 @@ class SimLWBManager:
     def __init__(self, node: 'SimNode'):
         self.node = node
 
-        self.rosunds = []
+        self.rounds = []
         self.gloria_manager = SimGloriaFlood(self.node)
 
     def generate_initial_schedule(self):
