7f9c1a8c79224eb51654415fdb9e30bb92f84df8
Dan Mugioiu <d.mugioiu@gmail.com>
2018-07-29 20:31:03 +0200

    Add file dialog button

--- flora_tools/trace_visualizer/static/main.js
+++ flora_tools/trace_visualizer/static/main.js
@@ -5,15 +5,46 @@ $(function () {
         // Great success!
 
         // Setup the dnd listeners.
-        var dropZone = document.getElementsByTagName('body')[0];
+        let dropZone = $('body').get(0);
         dropZone.addEventListener('dragover', handleDragOver, false);
         dropZone.addEventListener('drop', handleJSONDrop, false);
+
+        $('input[type="file"]').change(handleFileSelect);
 
 
     } else {
         alert('The File APIs are not fully supported in this browser.');
     }
 });
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    for (let i = 0, f; f = files[i]; i++) {
+
+        // Only process json files.
+        if (!f.type.match('application/json')) {
+            console.log("Not a JSON file!");
+            continue;
+        }
+
+        let reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function (theFile) {
+            return function (e) {
+                let trace = JSON.parse(e.target.result);
+                let svg = $().get();
+                let $hint = $('#visualization.visualization .dragndrop_hint');
+                $hint.hide();
+                new Timeline('#visualization .timeline', trace);
+            };
+        })(f);
+
+        reader.readAsText(f);
+    }
+}
+
 
 function handleJSONDrop(evt) {
     evt.stopPropagation();
