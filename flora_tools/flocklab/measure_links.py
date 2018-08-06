0530b081ccf8be7f53770a521d7d48aa0d38a323
Markus Wegmann <atokulus@gmail.com>
2018-08-06 11:53:28 +0200

    Sweep preambles

--- flora_tools/flocklab/measure_links.py
+++ flora_tools/flocklab/measure_links.py
@@ -2,12 +2,16 @@ import logging
 import os
 import time
 
+import pandas as pd
+
 import flora_tools.lwb_slot as lwb_slot
 from flora_tools.node import Node
 from flora_tools.radio_configuration import RadioConfiguration
 from flora_tools.radio_math import RadioMath
 
 from flora_tools.flocklab.flocklab import FlockLab
+
+
 
 ITERATIONS = 5
 
@@ -40,7 +44,7 @@ class MeasureLinksExperiment:
         self.disconnect_nodes()
 
     def connect_nodes(self):
-        self.nodes = [Node(flocklab=True, id=id) for id in FLOCKLAB_TARGET_ID_LIST]
+        self.nodes = [Node(flocklab=True, id=id, test=False) for id in FLOCKLAB_TARGET_ID_LIST]
 
         for node in self.nodes:
             node.interactive_mode(False)  # Enable single-line JSON Output
@@ -52,24 +56,30 @@ class MeasureLinksExperiment:
 
     def iterate_from_node(self, tx_node):
         for i in range(ITERATIONS):
-            for modulation in lwb_slot.RADIO_MODULATIONS:
-                for power in [0, 10, 22]:  # lwb_slot.POWERS:
-                    self.logger.info("Running Tx on Node: {}, Mod: {}, Power: {}".format(tx_node.id, modulation, power))
+            for preamble_len in [2, 3, 4, 6, 8, 10, 12]:
+                for modulation in lwb_slot.RADIO_MODULATIONS:
+                    for power in [0, 10, 22]:  # lwb_slot.POWERS:
+                        self.logger.info(
+                            "Tx on Node {}: Mod: {}, Power: {}, Preamble_Length: {}".format(
+                                tx_node.id, modulation, power, preamble_len))
 
-                    config = RadioConfiguration(modulation)
-                    math = RadioMath(config)
+                        config = RadioConfiguration(modulation, preamble=preamble_len)
+                        math = RadioMath(config)
 
-                    for node in self.nodes:
-                        self.configure_node(node, (node.id == tx_node.id), modulation, power)
-                        if node.id != tx_node.id:
-                            self.receive(node)
-                    time.sleep(0.2)
-                    self.send(tx_node)
-                    time.sleep(math.get_message_toa(len("Hello World!") + 1) * 2 + 0.1)
+                        message = "Hello World! from FlockLab Node {}: Mod: {}, Pow: {}, Prmbl: {}".format(
+                            tx_node.id, modulation, power, preamble_len)
+
+                        for node in self.nodes:
+                            self.configure_node(node, (node.id == tx_node.id), modulation, power, preamble_len)
+                            if node.id != tx_node.id:
+                                self.receive(node)
+                        time.sleep(0.1)
+                        self.send(tx_node, message=message)
+                        time.sleep(math.get_message_toa(len(message) + 1) * 1.5 + 0.1)
 
     @staticmethod
-    def configure_node(node, tx: bool, modulation, power):
-        config = RadioConfiguration(modulation, power=power, tx=tx)
+    def configure_node(node, tx: bool, modulation, power, preamble: int):
+        config = RadioConfiguration(modulation, power=power, tx=tx, preamble=preamble)
         node.cmd(config.cmd)
 
     @staticmethod
@@ -77,5 +87,18 @@ class MeasureLinksExperiment:
         node.cmd('radio receive')
 
     @staticmethod
-    def send(node):
-        node.cmd('radio send "Hello World!"')
+    def send(node, message):
+        node.cmd('radio send "{}"'.format(message))
+
+    @staticmethod
+    def parse_serial_log(file):
+        with open(file) as f:
+            content = f.readlines()
+
+            for line in content:
+                line.split()
+
+    @staticmethod
+    def analyze():
+        pass
+
